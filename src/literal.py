317ab9d372b4576e2006a07d09f83b8abbee9780
thor <thorck@protonmail.com>
2022-06-13 11:17:12 -0400

    minor refactors

--- src/literal.py
+++ src/literal.py
@@ -3,7 +3,7 @@ from typing_extensions import Self
 NINETY_FIVE = 95  # todo: why 95
 
 class Literal():
-    bytess: bytes
+    byte_v: bytes
 
     def __init__(self, value: str | int):
         value = str(value)
@@ -11,38 +11,35 @@ class Literal():
         value.strip()
         value.strip('_')
         value.lstrip('0x')
-        bytess: bytes = value.encode('utf-8')
+        byte_v: bytes = value.encode('utf-8')
 
-        if len(bytess) <= 32:
-            self.bytes = bytess
+        if len(byte_v) <= 32:
+            self.bytes = byte_v
             # todo: test for incorrect encoding
         else:
-            raise(OverflowError(f"literal {int(bytess)} overflows uint256"))
+            raise(OverflowError(f"literal {int(byte_v)} overflows uint256"))
 
     def __repr__(self) -> str:
-        # print(str(self.bytes))
-        return str(self.bytess.decode('utf-8'))
+        return str(self.byte_v.decode('utf-8'))
 
-    def __add__(self, right):
-        # print(str(self.bytes))
-        return str(self.bytess.decode('utf-8'))
+    def __add__(self, right: Self) -> Self:
+        return Literal(int(self.byte_v)+ int(right.byte_v))
 
-    def __sub__(self, right):
-        pass
+    def __sub__(self, right) -> Self:
+        return Literal(int(self.byte_v)- int(right.byte_v))
 
-    def __mul__(self, right):
-        pass
+    def __mul__(self, right) -> Self:
+        return Literal(int(self.byte_v)* int(right.byte_v))
 
-    def __floordiv__(self, right):
-        pass
+    def __floordiv__(self, right) -> Self:
+        return Literal(int(self.byte_v)// int(right.byte_v))
 
-    def to_hex(self) -> bytes:
-        # print(str(self.bytes))
-        return self.bytess
+    # def to_hex(self) -> bytes:
+    #     pass
 
     @classmethod
-    def push_n(cls: type[Self], n: int):
+    def push_n(cls: type[Self], n: int) -> Self:
         if n < 1 or n > 32:
             raise ValueError(f"can't push value {n}; not in range [1,32]")
         else:
-            return Literal(NINETY_FIVE + n)
+            return cls(NINETY_FIVE + n)
--- src/macros.py
+++ src/macros.py
@@ -1,33 +1,28 @@
-import enum
 from literal import Literal
 from value import ValueEnum
-
-class Macro():
-    def __init__(self, name: str, idx: int, args: list[str], content: list[ValueEnum]):
-        if len(args) == 1 and args[0] == "":
-            self.args = []
-        else:
-            self.args = args
-        self.name = name
-        self.idx = idx
-        self.content = content
+from value_utils.types import JumpTable
 
 
-class JumpTable():
-    def __init__(self, name: str, labels: list[str]):
-        self.name = name
-        self.labels = labels
+class MacroEnum():
+    class Macro:
+        name: str
+        idx: int
+        args: list[str]
+        content: list[ValueEnum]
 
+    values: None | list[ValueEnum] = None
+    macro: None | Macro = None
+    jumptable: None | JumpTable = None
 
-# Yikes, python enums
-class MacroEnum(enum.Enum):
-    VALUES = 1
-    MACRO = 2
-    JUMPTABLE: JumpTable
+    def __init__(self,
+                 name: str,
+                 values: list[ValueEnum] | None = None,
+                 macro: None | Macro = None,
+                 jumptable: None | JumpTable = None):
+        assert name in ["VALUES", "MACRO", "JUMPTABLE"]
+        assert values is not None or macro is not None or jumptable is not None
+        self._name_ = name
+        self.values = values
+        self.macro = macro
+        self.jumptable = jumptable
 
-    def __new__(cls, variant: str, **kwargs):
-        cls._name_ = variant
-        cls._value_ = {"VALUES": ValueEnum(**kwargs),
-                       "MACRO": Macro(**kwargs),
-                       "JUMPTABLE": JumpTable(**kwargs)
-                       }[variant]
